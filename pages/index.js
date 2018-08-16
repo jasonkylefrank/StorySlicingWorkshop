4d9aac53dca616fea0366c31bf7b1a193a3ce22d
Jason K. Frank <jasonkylefrank@gmail.com>
2018-08-16 10:33:33 -0400

    Refactor 3: Created method to render input rows to make render() more DRY

--- pages/index.js
+++ pages/index.js
@@ -13,25 +13,25 @@ class Index extends React.Component {
         this.setState({[key]: event.target.value});
     }
 
+    renderInput = (labelText, stateKey) => {
+        return (
+            <div>
+                <label>{labelText}</label>
+                <input type="text" onChange={event => this.onChange(stateKey, event)}></input>                   
+            </div>
+        );
+    }
+
     render() {
         const { name, address, phone } = this.state;
         console.log("In render, the name is:" + name);
         return (
             <div>
                 <h2>Welcom to HAMS!</h2>
-                <div>
-                    <label>Name:</label>
-                    <input type="text" onChange={event => this.onChange("name", event)}></input>                   
-                </div>
-
-                <div>
-                    <label>Address:</label>
-                    <input type="text" onChange={event => this.onChange("address", event)}></input>
-                </div>
-                <div>
-                    <label>Phone:</label>
-                    <input type="text" onChange={event => this.onChange("phone", event)}></input>                   
-                </div>
+                
+                {this.renderInput("Name:", "name")}
+                {this.renderInput("Address:", "address")}
+                {this.renderInput("Phone:", "phone")}
 
                 <br />
                 <h4>Your data from DB:</h4>
